2b3592414cabbb011ebb55516a44eb3f58e3e2a7
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2025-12-25 23:40:41 +0530

    added new cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import './App.css'
 import loud from './assets/loud.png'
 import shine from './assets/star.webp'
 import arrow from './assets/arrow.webp'
+import classes from './assets/classes.webp';
+import JEE from './assets/JEE Courses.webp';
+import NEET from './assets/Neet Courses.webp';
 // import { BrowserRouter, Router, Route } from 'react-router-dom';
 const App = () => {
   return (
     <div className='wrapper'>
       <Nav></Nav>
-      <div style={{width: '100%', height: '40px', backgroundColor: '#019370', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <div style={{width: '100%', height: '40px', backgroundColor: '#019370', display: 'flex', justifyContent: 'center', alignItems: 'center', userSelect: 'none'}}>
         <div style={{display: 'flex', alignItems: 'center'}}>
           <img width={32} draggable={false} src={loud} alt={"Speaker"} style={{aspectRatio: '1/1'}} />
           <span style={{color: '#F8FD05', fontSize: '12px', fontWeight: 600}}>LAST CHANCE TO GET 15% OFF</span>
@@ -23,13 +26,51 @@ const App = () => {
       </div>
       <div style={{backgroundColor: '#edf2fa', display: 'flex', flexDirection: 'column', paddingBottom: '50px'}}>
         <Carousel></Carousel>
-        <div style={{display: 'flex', gap: '16px', justifyContent: 'center', marginTop: '24px'}}>
+        <div style={{display: 'flex', gap: '16px', justifyContent: 'center', marginTop: '14px'}}>
           <div style={{borderRadius: '50%', width: '12px', height: '12px', backgroundColor: '#1C1D1D', cursor: 'pointer'}}></div>
           <div style={{borderRadius: '50%', width: '12px', height: '12px', backgroundColor: '#0F0F0F33', cursor: 'pointer'}}></div>
           <div style={{borderRadius: '50%', width: '12px', height: '12px', backgroundColor: '#0F0F0F33', cursor: 'pointer'}}></div>
           <div style={{borderRadius: '50%', width: '12px', height: '12px', backgroundColor: '#0F0F0F33', cursor: 'pointer'}}></div>
         </div>
       </div>
+
+      <div style={{width: '1032px', display: 'flex', flexDirection: 'column', margin: '0 auto', userSelect: 'none'}}>
+        <h2 style={{margin: 0, marginTop: '52px', fontSize: '24px'}}>Pick the right course for you</h2>
+        <div style={{padding: '24px 0 0 0', display: 'flex', backgroundColor: 'white', gap: '36px', paddingBottom: '50px'}}>
+          <div style={{backgroundColor: '#edf2fa', width: '100%', borderRadius: '20px', padding: '24px', height: '192px', display: 'flex', justifyContent: 'space-between', flexDirection: 'column', position: 'relative'}}>
+            <h3 style={{fontSize: '20px', margin: 0}}>NEET Courses</h3>
+            <img style={{position: 'absolute', width: '315px', bottom: 0, right: 0}} src={NEET} />
+            <div style={{display: 'flex'}}>
+              <h2 style={{fontSize: '16px', color: '#025FCA', cursor: 'pointer', margin: 0, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', marginBottom: '3px', zIndex: 1}}>View Courses </h2>
+              <svg style={{marginTop: '16px', cursor: 'pointer', zIndex: 1}} fill={"none"} height={"24" }viewBox={"0 0 20 20"} width={"24"} xmlns={"http://www.w3.org/2000/svg"}><path d={"M7.5 15L12.5 10L7.5 5"} stroke={"#0266DA"} stroke-linecap={"round"} stroke-linejoin={"round"} stroke-width={"1.5"}></path></svg>
+            </div>
+            
+          </div>
+          <div style={{backgroundColor: '#edf2fa', width: '100%', borderRadius: '20px', padding: '24px', height: '192px', display: 'flex', justifyContent: 'space-between', flexDirection: 'column', position: 'relative'}}>
+            <h3 style={{fontSize: '20px', margin: 0}}>JEE Courses</h3>
+            <img style={{position: 'absolute', width: '315px', bottom: 0, right: 0}} src={JEE} />
+            <div style={{display: 'flex'}}>
+              <h2 style={{fontSize: '16px', color: '#025FCA', cursor: 'pointer', margin: 0, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', marginBottom: '3px', zIndex: 1}}>View Courses </h2>
+              <svg style={{marginTop: '16px', cursor: 'pointer', zIndex: 1}} fill="none" height="24" viewBox="0 0 20 20" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 15L12.5 10L7.5 5" stroke="#0266DA" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path></svg>
+            </div>
+            
+          </div>
+          <div style={{backgroundColor: '#edf2fa', width: '100%', borderRadius: '16px', padding: '24px', height: '192px', display: 'flex', justifyContent: 'space-between', flexDirection: 'column', position: 'relative'}}>
+            <h3 style={{fontSize: '20px', margin: 0}}>Courses for class 6-10</h3>
+            <img style={{position: 'absolute', width: '315px', bottom: 0, right: 0}} src={classes} />
+            <div style={{display: 'flex'}}>
+              <h2 style={{fontSize: '16px', color: '#025FCA', cursor: 'pointer', margin: 0, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', marginBottom: '3px', zIndex: 1}}>View Courses </h2>
+              <svg style={{marginTop: '16px', cursor: 'pointer', zIndex: 1}} fill="none" height="24" viewBox="0 0 20 20" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 15L12.5 10L7.5 5" stroke="#0266DA" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path></svg>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+      <div style={{backgroundColor: '#f7f9ff', width: '100%', height: '114px'}}>
+
+      </div>
+
     </div>
   )
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+// import frame1 from '../assets/frame1.webp'
+// import frame2 from '../assets/frame2.webp'
+// import frame3 from '../assets/frame3.webp'
+// import frame4 from '../assets/frame4.webp'
 
 export const Carousel = () => {
+    // const [frontPhoto, setfrontPhoto] = useState(frame1);
+    // const carouselArr = [];
+
   return (
     <div style={{width: '1032px', margin: '0 auto', height: '350px', marginTop: '80px', borderRadius: '34px', backgroundColor: '#74f93264'}}>
 
