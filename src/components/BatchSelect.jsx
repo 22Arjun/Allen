d394021d8cafb5f7fc759438f88778ee9a5b6861
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2025-12-31 20:15:34 +0530

    finishing positioning the Trending Courses Card"

--- src/components/BatchSelect.jsx
+++ src/components/BatchSelect.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const BatchSelect = () => {
   return (
-    <div style={{width: '312px', height: '292.5px', padding: '1rem', position: 'relative', backgroundColor: 'white', borderRadius: '1rem'}}>
+    <div style={{width: '312px', height: '320.5px', padding: '1rem', position: 'relative', backgroundColor: 'white', borderRadius: '1rem'}}>
         <span style={{backgroundColor: '#F66A05', display: 'block', position: 'absolute', top: '1rem', right: '-.5rem', padding: '.25rem .75rem .25rem .625rem', color: 'white', lineHeight: '.875rem', borderTopRightRadius: '.375rem', borderTopLeftRadius: '.75rem', borderBottomLeftRadius: '.75rem'}}>
             <div style={{fontSize: '10px', fontWeight: '700', position: 'relative'}}>FINAL CHANCE: 10% OFF
                 <div style={{position: 'absolute', width: '0', height: '0', right: '-.75rem', bottom: '-.75rem', opacity: '.5', borderRight: '8px solid transparent', borderLeft: '8px solid transparent', borderTop: '8px solid #f66A05', transform: 'translate(2px, -2px) rotate(135deg) scale(0.72)'}}></div>
@@ -19,21 +19,21 @@ export const BatchSelect = () => {
             </div>
             <p style={{lineHeight: '1.5rem', marginTop: '0.75rem', fontWeight: 600, fontSize: '1rem', marginBottom: '0'}}>Nurture Online Courses</p>
             <p style={{fontSize: '.875rem', color: '#494a4a', fontWeight: 400, lineHeight: '1.375rem', margin: '4px 0 0 '}}>Target 2028</p>
-            <div style={{margin: '16px 0 0', display: 'flex', gap: '8px', paddingRight: '38px'}}>
+            <div style={{margin: '16px 0 0', display: 'flex', gap: '8px', paddingRight: '22px'}}>
                 <div>
                     <svg fill="none" height="22" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg"><mask height="22" id="mask0_7026_59778" maskUnits="userSpaceOnUse" width="16" x="0" y="0" style={{maskType: 'alpha'}}><path d="M0 0H16V16H0z" fill="#D9D9D9"></path></mask><g mask="url(#mask0_7026_59778)"><path d="M6.483 9.85l4.984-4.983a.584.584 0 01.85 0 .585.585 0 01.006.846l-5.406 5.42a.584.584 0 01-.85 0L3.683 8.75a.583.583 0 010-.85.586.586 0 01.85 0l1.95 1.95z" fill="#494A4A"></path></g></svg>
                 </div>
                 <p style={{fontSize: '.875rem', lineHeight: '1.375rem', color: '#494a4a', letterSpacing: '.0175rem', fontWeight: 400, margin: 0}}>800+ hrs. of LIVE classes with ALLEN Kota's top faculty</p>
             </div>
 
-            <div style={{margin: '16px 0 0', display: 'flex', gap: '8px', paddingRight: '38px'}}>
+            <div style={{margin: '16px 0 0', display: 'flex', gap: '8px', paddingRight: '22px'}}>
                 <div>
                     <svg fill="none" height="22" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg"><mask height="22" id="mask0_7026_59778" maskUnits="userSpaceOnUse" width="16" x="0" y="0" style={{maskType: 'alpha'}}><path d="M0 0H16V16H0z" fill="#D9D9D9"></path></mask><g mask="url(#mask0_7026_59778)"><path d="M6.483 9.85l4.984-4.983a.584.584 0 01.85 0 .585.585 0 01.006.846l-5.406 5.42a.584.584 0 01-.85 0L3.683 8.75a.583.583 0 010-.85.586.586 0 01.85 0l1.95 1.95z" fill="#494A4A"></path></g></svg>
                 </div>
                 <p style={{fontSize: '.875rem', lineHeight: '1.375rem', color: '#494a4a', letterSpacing: '.0175rem', fontWeight: 400, margin: 0}}>Upto 20 Online Tests</p>
             </div>
 
-            <div style={{margin: '16px 0 0', display: 'flex', gap: '8px', paddingRight: '38px'}}>
+            <div style={{margin: '16px 0 0', display: 'flex', gap: '8px', paddingRight: '22px'}}>
                 <div>
                     <svg fill="none" height="22" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg"><mask height="22" id="mask0_7026_59778" maskUnits="userSpaceOnUse" width="16" x="0" y="0" style={{maskType: 'alpha'}}><path d="M0 0H16V16H0z" fill="#D9D9D9"></path></mask><g mask="url(#mask0_7026_59778)"><path d="M6.483 9.85l4.984-4.983a.584.584 0 01.85 0 .585.585 0 01.006.846l-5.406 5.42a.584.584 0 01-.85 0L3.683 8.75a.583.583 0 010-.85.586.586 0 01.85 0l1.95 1.95z" fill="#494A4A"></path></g></svg>
                 </div>
@@ -41,15 +41,27 @@ export const BatchSelect = () => {
             </div>
             
         </div>
-        <div>
-            <div>
-                <span>
+        <div style={{marginTop: '2rem', position: 'relative'}}>
+            <div style={{display: 'flex'}}>
+                <span style={{fontSize: '.75rem', color: '#757575', position: 'relative'}}>
                     ₹85,000
-                    <span></span>
+                    <span style={{width: '53px', height: '1px', backgroundColor: '#f44336', transform: 'rotate(-8deg) translate(-52px, -12px)', display: 'inline-block' }}></span>
                 </span>
-                <span></span>
+                <span style={{backgroundColor: '#35B58E', padding: '2px 10px', color: 'white', borderRadius: '9999px', fontSize: '.625rem', lineHeight: '.875rem', fontWeight: '600', margin: 0, position: 'absolute', transform: 'translate(60px, -1px)'}}>10% OFF</span>
             </div>
+            <div style={{marginTop: '2px'}}>
+                <h3 style={{lineHeight: '1.25rem', fontSize: '1rem', color: '#1e1e22', margin: 0, padding: 0}}>
+                    ₹76,500
+                    <label style={{fontSize: '12px', fontWeight: 600}}> + Taxes</label>
+                </h3>
+            </div>
+
+            <div style={{display: 'flex', position: 'absolute', top: 0, right: 0}}>
+                <h2 style={{fontSize: '16px', color: '#025FCA', cursor: 'pointer', margin: 0, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', marginBottom: '3px', zIndex: 1}}>Know more </h2>
+                <svg style={{marginTop: '16px', cursor: 'pointer', zIndex: 1}} fill={"none"} height={"24" }viewBox={"0 0 20 20"} width={"24"} xmlns={"http://www.w3.org/2000/svg"}><path d={"M7.5 15L12.5 10L7.5 5"} stroke={"#0266DA"} stroke-linecap={"round"} stroke-linejoin={"round"} stroke-width={"1.5"}></path></svg>
+            </div> 
         </div>
+
     </div>
   )
 }
